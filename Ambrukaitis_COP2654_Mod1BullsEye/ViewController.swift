3b7a855a175a418bac0b1e7575b3bae8cd57e898
Matas Ambrukaitis <mambrukaitis@icloud.com>
2025-01-15 09:44:53 -0500

    Added a slider, the (rounded)  slider value is shown when the "Hit me!" button is pressed.

--- Ambrukaitis_COP2654_Mod1BullsEye/ViewController.swift
+++ Ambrukaitis_COP2654_Mod1BullsEye/ViewController.swift
@@ -9,25 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Variables
+    var sliderCurrentValue: Int = 0 //contains the current position of the guessing slider
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showAlert() {
+        let message = "The value of the slider is \(sliderCurrentValue)"
+        
         let alert = UIAlertController(
             title: "Hello, World!",
-            message: "This is my first app!",
+            message: message,
             preferredStyle: .alert)
         
         let action = UIAlertAction(
-            title: "Awesome",
+            title: "Ok",
             style: .default,
             handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-
     }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        sliderCurrentValue = lroundf(slider.value)
+    }
+        
+        
 }
 
