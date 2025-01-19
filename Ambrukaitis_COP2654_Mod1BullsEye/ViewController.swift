6ac642a30babed7fcd8a29f9404ad18a103f3e12
Matas Ambrukaitis <mambrukaitis@icloud.com>
2025-01-18 20:14:42 -0500

    Added the function to tell the user what score they are aiming for, and the alert now states the target value and slider value.

--- Ambrukaitis_COP2654_Mod1BullsEye/ViewController.swift
+++ Ambrukaitis_COP2654_Mod1BullsEye/ViewController.swift
@@ -11,20 +11,26 @@ class ViewController: UIViewController {
 
     //Variables
     var sliderCurrentValue: Int = 0 //contains the current position of the guessing slider
-    
+    var targetValue: Int = 0
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        sliderCurrentValue = lroundf(slider.value)
+        startNewRound()
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is \(sliderCurrentValue)"
+        let message = "The value of the slider is: \(sliderCurrentValue)" +
+                      "\nThe target value was: \(targetValue)"
+                    
         
         let alert = UIAlertController(
             title: "Hello, World!",
             message: message,
             preferredStyle: .alert)
+        
         
         let action = UIAlertAction(
             title: "Ok",
@@ -33,12 +39,25 @@ class ViewController: UIViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         sliderCurrentValue = lroundf(slider.value)
     }
         
-        
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        sliderCurrentValue = 50
+        slider.value = Float(sliderCurrentValue) //resets the slider to 50
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
+    
 }
 
